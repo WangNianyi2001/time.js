469c202325b47855a5227049318931723e4c2aa8
Nianyi Wang <WangNianyi2001@outlook.com>
2019-03-09 23:04:31 +0800

    Implement class Task

--- Library.js
+++ Library.js
@@ -1,16 +1,21 @@
 {
 	'use strict';
 
+	const nulf = () => {};
+	const identity = obj => obj;
+
+	const parseNumber = obj => isNaN(obj) && obj || 0;
+
+	const getTimestamp = () => +new Date;
+
 	const give = properties => object => Object.keys(properties).forEach(
 		key => Object.defineProperty(object, key, Object.getOwnPropertyDescriptor(properties, key))
 	);
 
-	const identity = obj => obj;
-
 	const compose = (...fns) => !fns.length ? identity : ((outer, inner) => (...args) => outer(inner(...args)))(fns.shift(), compose(...fns));
 	const composeRight = (...fns) => !fns.length ? identity : ((outer, inner) => (...args) => outer(inner(...args)))(fns.pop(), composeRight(...fns));
 
-	const primitive = (map => obj => map[typeof obj] || obj === null || false)({
+	const isPrimitive = (map => obj => map[typeof obj] || obj === null || false)({
 		number: true,
 		string: true,
 		symbol: true,
@@ -22,14 +27,14 @@
 		if(typeof a !== typeof b) {
 			return false;
 		}
-		if([a, b].every(primitive)) {
+		if([a, b].every(isPrimitive)) {
 			// +0 !== -0
 			if(a.__proto__ === Number.prototype) {
 				return 1 / a === 1 / b;
 			}
 			return a === b;
 		}
-		// Rule out random objects
+		// Rule out objects that are definitely unequal
 		if(
 			a.__proto__ !== b.__proto__ ||
 			a.prototype !== b.prototype ||
@@ -40,13 +45,18 @@
 		const [a_des, b_des] = [a, b].map(
 			obj => Object.keys(Object.getOwnPropertyDescriptor(obj))
 		);
+		// TODO
 	};
 
 	const Library = {
+		nulf,
+		identity,
+		parseNumber,
+		getTimestamp,
 		give,
 		compose,
 		composeRight,
-		primitive,
+		isPrimitive,
 		equal: (a, b) => equal(a, b, [], []),
 	};
 
